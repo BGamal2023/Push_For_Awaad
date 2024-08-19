9942cb628bc29f7910c0b9d54737abf355268b3e
bahaa <malkebahaa450>
2024-08-19 21:08:52 +0300

    commit_10 stable commit

---
+++ C#/Car_GameBoy/Car_GameBoy/__Globals/Awaad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_GameBoy.__Globals
+{
+    internal class Awaad
+    {
+        public int a = 10;
+    }
+}
